f6008fd07cbede0658524d5a9c374a05014fc464
gary909 <garywhite61@hotmail.com>
2022-11-21 12:46:37 +0100

    code

---
+++ app.js
@@ -0,0 +1,9 @@
+function odds(values) {
+    // arrow it
+    return values.filter( => );
+}
+
+console.log(odds([])); //[]
+console.log(odds([2, 4, 6])); //[]
+console.log(odds([1, 3, 5])); //[1, 3, 5]
+console.log(odds([1, 2, 3, 4, 5, 6])); //[1, 3, 5]
